e6bc7090fcfbc721da1468f533830b1fe61c352b
alisha.raizada <alisharai1627@gmail.com>
2023-05-24 16:53:26 +0530

    Added extent report and screenshots on test failure

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,6 +1,13 @@
 package stepDefinitions;
+import java.io.File;
 import java.io.IOException;
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
 import utils.TestSetUp;
 
 public class Hooks {
@@ -20,6 +27,19 @@ public class Hooks {
     public void AfterScenario() throws IOException {
 
         testSetup.baseclass.WebDriverManager().quit();
+
+    }
+
+    //will get executed when scenario fails at any step
+    @AfterTest
+    public void TakeScreenshot(Scenario scenario) throws IOException {
+        WebDriver driver = testSetup.baseclass.WebDriverManager();
+        if(scenario.isFailed()){
+
+            File path = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            byte[] fileContent= FileUtils.readFileToByteArray(path);
+            scenario.attach("fileContent", "image/png" , "image");
+        }
 
     }
 
--- target/test-classes/features/searchproduct.feature
+++
@@ -1,8 +0,0 @@
-Feature: Search and place the order for vegetable
-
-Scenario: User should have same search experience for product search in home page and offers page
-Given User is on Greenkart landing page
-When User searched with shortname "Tom" and extract actual name of product
-Then User go and search for "Tom" short name in offers page to check if offer exists
-And User validates product name on landing page with offers page
-
